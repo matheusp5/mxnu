ecda05e13b311ecd8c6711c22ea371f88e1ba41b
ofmxtheuuz <mxtheuzchika@gmail.com>
2023-12-26 15:20:02 -0300

    feat: created main class

---
+++ Mxnu.Core/Main.cs
@@ -0,0 +1,13 @@
+﻿using GTA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mxnu.Core
+{
+    public class Main : Script
+    {
+    }
+}
