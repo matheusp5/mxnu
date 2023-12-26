31a91134314bf39e271b791aaccee21dd0d174dd
ofmxtheuuz <mxtheuzchika@gmail.com>
2023-12-26 15:20:44 -0300

    feat: properties

--- Mxnu.Core/Main.cs
+++ Mxnu.Core/Main.cs
@@ -1,13 +1,41 @@
 ﻿using GTA;
+using LemonUI.Menus;
+using LemonUI;
 using System;
-using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Security.Principal;
+using GTA.Math;
+using GTA.Native;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mxnu.Core
 {
     public class Main : Script
     {
+        private Player player { get; set; } = Game.Player;
+        public ObjectPool pool { get; set; }
+        public NativeMenu menu { get; set; }
+        public NativeMenu moneyMenu { get; set; }
+        public NativeMenu playerMenu { get; set; }
+        public NativeMenu weaponsMenu { get; set; }
+        public NativeMenu vehicleMenu { get; set; }
+        public NativeMenu weatherMenu { get; set; }
+        public NativeListItem<int> runSpeedItem { get; set; }
+        public NativeListItem<int> swimSpeedItem { get; set; }
+        public WeaponHash[] weapons { get; set; } = Weapon.GetAllWeaponHashesForHumanPeds();
+        public bool explosiveBullets { get; set; } = false;
+        public bool fireBullets { get; set; } = false;
+        public bool multiplierEnabled { get; set; } = false;
+        public float runSpeedMultipler { get; set; } = 1;
+        public float swimSpeedMultipler { get; set; } = 1;
+        public bool noClipEnable { get; set; } = false;
+        public int noClipSpeed { get; set; } = 2;
+        public float noClipedX { get; set; }
+        public float noClipedY { get; set; }
+        public float noClipedZ { get; set; }
+        public bool ignoredByPoliceEnable { get; set; } = false;
+        public bool superJumpEnable { get; set; } = false;
+        public bool alwaysClean { get; set; } = false;
+        public NativeListItem<int> customHourItem { get; set; }
     }
 }
